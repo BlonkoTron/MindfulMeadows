c3ba1fed6c3f946f34d947ce787752035db1454a
ContaDoge <mgerst23@student.aau.dk>
2024-03-12 14:11:48 +0100

    Bunch of Menu & Setting stuff

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -6,6 +6,9 @@ using UnityEngine.SceneManagement;
 public class MenuButtons : MonoBehaviour
 {
     public GameObject SettingMenu;
+    public GameObject CurrentMenu;
+    public GameObject GameSetting;
+    public GameObject AudioSetting;
 
     public void StartGame()
     {
@@ -29,6 +32,8 @@ public class MenuButtons : MonoBehaviour
         SceneManager.LoadScene("Main Menu");
         Debug.Log("Loading Menu");
     }
+
+    // Settings Menu
     public void SettingsMenu()
     {
         if (SettingMenu.activeSelf)
@@ -41,5 +46,32 @@ public class MenuButtons : MonoBehaviour
           //  GameManager.instance.Pause();
             SettingMenu.SetActive(true);
         }
+    }
+    void SwitchMenu(GameObject newMenu)
+    {
+        if (CurrentMenu != null)
+        {
+            CurrentMenu.SetActive(false);
+        }
+
+        newMenu.SetActive(true);
+        CurrentMenu = newMenu;
+    }
+    public void GameSettings()
+    {
+       // GameManager.instance.Pause();
+        SwitchMenu(GameSetting);
+    }
+    public void AudioSettings()
+    {
+        // GameManager.instance.Pause();
+        SwitchMenu(AudioSetting);
+    }
+
+    public void ExitSettings()
+    {
+       // GameManager.instance.Resume();
+        CurrentMenu.SetActive(false);
+        CurrentMenu = null;
     }
 }
