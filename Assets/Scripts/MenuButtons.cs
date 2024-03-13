0d771e2bb3b3ecc7a5480b474e327102d7a8857a
ContaDoge <mgerst23@student.aau.dk>
2024-03-13 15:19:44 +0100

    BUNCH OF UI SHIT

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -5,11 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
 {
-    public GameObject SettingMenu;
-    public GameObject CurrentMenu;
-    public GameObject GameSetting;
-    public GameObject AudioSetting;
 
+    [SerializeField] private GameObject SettingMenu;
+    [SerializeField] private GameObject EscapeMenu;
+    [SerializeField] private GameObject CurrentMenu;
+    [SerializeField] private GameObject GameSetting;
+    [SerializeField] private GameObject AudioSetting;
+    [SerializeField] private GameObject VideoSetting;
+    [SerializeField] private GameObject ControlSetting;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            EscapeMenuBool();
+        }
+    }
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -47,6 +58,21 @@ public class MenuButtons : MonoBehaviour
             SettingMenu.SetActive(true);
         }
     }
+    public void EscapeMenuBool()
+    {
+        if (EscapeMenu.activeSelf)
+        {
+            //   GameManager.instance.Resume();
+            EscapeMenu.SetActive(false);
+        }
+        else
+        {
+            //  GameManager.instance.Pause();
+            EscapeMenu.SetActive(true);
+        }
+    }
+
+    // Switching between settings
     void SwitchMenu(GameObject newMenu)
     {
         if (CurrentMenu != null)
@@ -57,21 +83,21 @@ public class MenuButtons : MonoBehaviour
         newMenu.SetActive(true);
         CurrentMenu = newMenu;
     }
+    public void ExitSettings()
+    {
+       // GameManager.instance.Resume();
+        CurrentMenu.SetActive(false);
+        CurrentMenu = null;
+    }
+
     public void GameSettings()
     {
-       // GameManager.instance.Pause();
+        // GameManager.instance.Pause();
         SwitchMenu(GameSetting);
     }
     public void AudioSettings()
     {
         // GameManager.instance.Pause();
         SwitchMenu(AudioSetting);
-    }
-
-    public void ExitSettings()
-    {
-       // GameManager.instance.Resume();
-        CurrentMenu.SetActive(false);
-        CurrentMenu = null;
     }
 }
