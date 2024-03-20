4930da0637cbcfcf01cd2c8814bdc7539315882b
oscarbagger <oscarbagger@live.dk>
2024-03-20 13:47:49 +0100

    Bad area

---
+++ Assets/BadArea.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadArea : MonoBehaviour
+{
+    [HideInInspector] public int treesPlantedHere = 0;
+
+}
