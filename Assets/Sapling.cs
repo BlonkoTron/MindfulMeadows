ba26e340442b3d5088be05cc89a65931b67226ac
BlonkoTron <133660863+BlonkoTron@users.noreply.github.com>
2024-03-21 11:23:00 +0100

    sapling does in fact do the growing

---
+++ Assets/Sapling.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sapling : MonoBehaviour
+{
+
+    [SerializeField] private float growTime;
+    [SerializeField] private GameObject treePrefab;
+
+    private float currentGrowTime;
+    private bool canGrow = true;
+
+    private void Start()
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(0, Random.Range(0, 360), 0));
+        currentGrowTime = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        if (currentGrowTime < growTime)
+        {
+            currentGrowTime += Time.fixedDeltaTime;
+        }
+        else
+        {
+            if (canGrow)
+            {
+                Destroy(gameObject);
+            }
+           
+        }
+    }
+
+    private void OnDestroy()
+    {
+        Instantiate(treePrefab, transform.position, transform.rotation);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canGrow = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canGrow = true;
+        }
+    }
+
+}
