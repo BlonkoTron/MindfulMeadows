bfb98871c1479de08ff6bf89d972c28d80de3d8d
ContaDoge <mgerst23@student.aau.dk>
2024-03-21 15:58:06 +0100

    Update StressBar.cs

--- Assets/Scripts/UI/StressBar.cs
+++ Assets/Scripts/UI/StressBar.cs
@@ -11,55 +11,54 @@
         public Image stressImage;
         public Text stressText;
 
-
-        void Start()
+    void Start()
+    {
+        currentStress = 0;
+    }
+    void Update()
+    {
+        AddStress(0.1f);
+    }
+    public void AddStress(float stress)
+    {
+        currentStress += stress;
+        if (currentStress > maxStress)
+        {
+            currentStress = maxStress;
+        }
+        UpdateStressBar();
+    }
+    public void RemoveStress(float stress)
+    {
+        currentStress -= stress;
+        if (currentStress < 0)
         {
             currentStress = 0;
         }
-        void Update()
-        {
-            AddStress(0.1f);       
-        }
-        public void AddStress(float stress)
-        {
-            currentStress += stress;
-            if (currentStress > maxStress)
-            {
-                currentStress = maxStress;
-            }
-            UpdateStressBar();
-        }
-        public void RemoveStress(float stress)
-        {
-            currentStress -= stress;
-            if (currentStress < 0)
-            {
-                currentStress = 0;
-            }
-            UpdateStressBar();
-        }
-        public float GetCurrentStress()
-        {
-            UpdateStressBar();
-            return currentStress;
-        }
-        public void UpdateStressBar()
-        {
-            float ratio = (float)currentStress / (float)maxStress;
-            int index = (int)(ratio * stressSprites.Length);
+        UpdateStressBar();
+    }
+    public float GetCurrentStress()
+    {
+        UpdateStressBar();
+        return currentStress;
+    }
+    public void UpdateStressBar()
+    {
+        float ratio = (float)currentStress / (float)maxStress;
+        int index = (int)(ratio * stressSprites.Length);
 
-            if (index >= stressSprites.Length)
-            {
-                index = stressSprites.Length - 1;
-            }
+        if (index >= stressSprites.Length)
+        {
+            index = stressSprites.Length - 1;
+        }
 
-            if (stressImage != null && stressSprites != null && stressSprites.Length > 0)
-            {
-                stressImage.sprite = stressSprites[index];
-            }
-                    if (stressText != null)
-            {
+        if (stressImage != null && stressSprites != null && stressSprites.Length > 0)
+        {
+            stressImage.sprite = stressSprites[index];
+        }
+        if (stressText != null)
+        {
             stressText.text = "Stress: " + currentStress.ToString();
-            }
         }
     }
+}
