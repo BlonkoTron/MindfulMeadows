3c85e0e69ca28d05dd85d769daeaf1b9cf8f1a2d
gigrove <147515425+gigrove@users.noreply.github.com>
2024-04-16 13:37:50 +0200

    Made the NPCS talk :))

--- Assets/Scripts/Talk.cs
+++ Assets/Scripts/Talk.cs
@@ -9,20 +9,25 @@ public class Talk : Interaction
     private protected DialogBox txtBox;
     public static float textSpeed=0.05f;
 
+    private AudioManager instance;
+
     private void Start()
     {
         interactionStages = dialogTexts.Length;
         txtBox = GameObject.FindGameObjectWithTag("dialogbox").GetComponent<DialogBox>();
+        instance = FindObjectOfType<AudioManager>();
     }
     public override void Interact()
     {
         if (!isInteracting)
         {
+            Debug.Log("Attempting to start interaction...");
             InteractionStart();
         } else
         {
             if (txtBox.writeTextDone)
             {
+                instance.Stop("npcvoice");
                 NextDialog();
             }
             else
@@ -33,17 +38,30 @@ public class Talk : Interaction
     }
     public override void InteractionStart()
     {
+        Debug.Log("Starting interaction...");
         isInteracting = true;
+        if (instance != null)
+        {
+            instance.Play("npcvoice", false);
+            Debug.Log("Playing voice...");
+        }
+        else
+        {
+            Debug.LogWarning("No Audio Manager assigned to NPC?");
+        }
+
         txtBox.ToggleBox(true);
         txtBox.SetNewText(dialogTexts[0], true);
+        Debug.Log("Text set...");
     }
     public void NextDialog()
     {
         // check if more dialog is available
-        if (myInteractionStage<dialogTexts.Length-1)
+        if (myInteractionStage < dialogTexts.Length-1)
         {
             // set new dialog
             myInteractionStage++;
+            instance.Play("npcvoice", false);
             txtBox.SetNewText(dialogTexts[myInteractionStage],true);
         } else
         {
@@ -53,6 +71,7 @@ public class Talk : Interaction
     }
     public override void InteractionEnd()
     {
+        instance.Stop("npcvoice");
         PlayerMovement.instance.canMove = true;
         isInteracting = false;
         myInteractionStage = 0;
