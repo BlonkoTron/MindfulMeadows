0098977aa1078f68df9cc943f32b0b1abb99c254
gigrove <147515425+gigrove@users.noreply.github.com>
2024-04-18 10:20:53 +0200

    ouhhhh... ermmmm....

---
+++ Assets/Scripts/TalkSecret.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+
+public class TalkSecret : Interaction
+{
+    [Multiline]
+    [SerializeField] private protected string[] dialogTexts;
+    private protected DialogBox txtBox;
+    public static float textSpeed = 0.05f;
+
+    private AudioManager instance;
+
+    private void Start()
+    {
+        interactionStages = dialogTexts.Length;
+        txtBox = GameObject.FindGameObjectWithTag("dialogbox").GetComponent<DialogBox>();
+        instance = FindObjectOfType<AudioManager>();
+    }
+    public override void Interact()
+    {
+        if (!isInteracting)
+        {
+            Debug.Log("Attempting to start interaction...");
+            InteractionStart();
+        }
+        else
+        {
+            if (txtBox.writeTextDone)
+            {
+                instance.Stop("secretvoice");
+                NextDialog();
+            }
+            else
+            {
+                txtBox.SetNewText(dialogTexts[myInteractionStage], false);
+            }
+        }
+    }
+    public override void InteractionStart()
+    {
+        Debug.Log("Starting interaction...");
+        isInteracting = true;
+        if (instance != null)
+        {
+            instance.Play("secretvoice", false);
+            Debug.Log("Playing voice...");
+        }
+        else
+        {
+            Debug.LogWarning("No Audio Manager assigned to NPC?");
+        }
+
+        txtBox.ToggleBox(true);
+        txtBox.SetNewText(dialogTexts[0], true);
+        Debug.Log("Text set...");
+    }
+    public void NextDialog()
+    {
+        // check if more dialog is available
+        if (myInteractionStage < dialogTexts.Length - 1)
+        {
+            // set new dialog
+            myInteractionStage++;
+            instance.Play("secretvoice", false);
+            txtBox.SetNewText(dialogTexts[myInteractionStage], true);
+        }
+        else
+        {
+            // end dialog
+            InteractionEnd();
+        }
+    }
+    public override void InteractionEnd()
+    {
+        instance.Stop("secretvoice");
+        PlayerMovement.instance.canMove = true;
+        isInteracting = false;
+        myInteractionStage = 0;
+        txtBox.ToggleBox(false);
+    }
+    public override void InteractionCancel()
+    {
+        PlayerMovement.instance.canMove = true;
+        isInteracting = false;
+        myInteractionStage = 0;
+        txtBox.ToggleBox(false);
+    }
+}
